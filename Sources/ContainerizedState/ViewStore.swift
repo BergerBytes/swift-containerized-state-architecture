347ed4ac5f114dbaa802b9669f8142da329269bf
Michael Berger <contact@bergerbytes.io>
2021-08-03 14:57:37 -0500

    Always dispatch render to main thread

--- Sources/ContainerizedState/ViewStore.swift
+++ Sources/ContainerizedState/ViewStore.swift
@@ -38,11 +38,11 @@ open class ViewStore<State: ViewState>: StateStore<State> {
                             : nil)
         }
         
-        if Thread.isMainThread {
-            renderBlock()
-        } else {
+//        if Thread.isMainThread {
+//            renderBlock()
+//        } else {
             DispatchQueue.main.async(execute: renderBlock)
-        }
+//        }
     }
     
     private func handleNotPossibleRender(error: RenderPolicy.RenderError, view: AnyStatefulView<State>) {
